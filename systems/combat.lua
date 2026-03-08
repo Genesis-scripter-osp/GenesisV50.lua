d51f4837647f690379df4cf41d905c2e887f5640
Genesis-scripter-osp <zatacutcanh2011@gmail.com>
2026-03-08 09:17:36 +0700

    Update combat.lua

--- systems/combat.lua
+++ systems/combat.lua
@@ -1,29 +1,12 @@
-local Combat = { FastAttack = true }
-function Combat:Attack()
-    local lp = game.Players.LocalPlayer
-    local tool = lp.Character and lp.Character:FindFirstChildOfClass("Tool")
-    if tool and self.FastAttack then
-        -- Gửi tín hiệu đánh nhanh lên Server
-        game:GetService("ReplicatedStorage").Modules.Net["RE/RegisterAttack"]:FireServer(tool)
-        -- Giả lập click chuột
-        game:GetService("VirtualUser"):Button1Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
-    end
-end
-function Combat:Init() 
-    _G.Genesis:Register("Combat", self)
-    game:GetService("RunService").Stepped:Connect(function() self:Attack() end)
-end
-return Combat
 local Combat = { FastAttack = false }
 function Combat:Attack()
     if self.FastAttack then
         local lp = game.Players.LocalPlayer
         local tool = lp.Character and lp.Character:FindFirstChildOfClass("Tool")
-        if tool and tool:IsA("Tool") then
-            -- Bypass cooldown để đánh siêu nhanh
+        if tool then
+            -- Gửi lệnh đánh liên tục lên server
             game:GetService("ReplicatedStorage").Modules.Net["RE/RegisterAttack"]:FireServer(tool)
-            game:GetService("VirtualUser"):CaptureController()
-            game:GetService("VirtualUser"):Button1Down(Vector2.new(1280, 672))
+            game:GetService("VirtualUser"):Button1Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
         end
     end
 end
