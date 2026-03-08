074337dbd8a6dc3b31f452e5e9e0ff08f9dd7eaa
Genesis-scripter-osp <zatacutcanh2011@gmail.com>
2026-03-08 11:18:07 +0700

    Create services.lua

---
+++ core/services.lua
@@ -0,0 +1,10 @@
+local Services = {}
+
+Services.Players = game:GetService("Players")
+Services.RunService = game:GetService("RunService")
+Services.TweenService = game:GetService("TweenService")
+Services.ReplicatedStorage = game:GetService("ReplicatedStorage")
+Services.HttpService = game:GetService("HttpService")
+Services.Workspace = game:GetService("Workspace")
+
+return Services
