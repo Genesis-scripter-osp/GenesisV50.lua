d45e469edd72fa3d67a1db17ff3b8c519d134aea
Genesis-scripter-osp <zatacutcanh2011@gmail.com>
2026-03-08 11:35:54 +0700

    Update GenesisV50.lua

--- GenesisV50.lua
+++ GenesisV50.lua
@@ -11,8 +11,9 @@ Genesis.Modules = {}
 
 local HttpService = game:GetService("HttpService")
 
---// Load Module Function
+-- Load Module
 function Genesis:LoadModule(name, url)
+
     local success, module = pcall(function()
         return loadstring(game:HttpGet(url))()
     end)
@@ -23,48 +24,50 @@ function Genesis:LoadModule(name, url)
     else
         warn("[Genesis] Failed:", name)
     end
+
 end
 
---// Base URL
+
+-- Base URL
 local BASE = "https://raw.githubusercontent.com/Genesis-scripter-osp/GenesisV50.lua/main/"
 
---// Load Modules
-Genesis:LoadModule("UI", BASE.."ui/ui.lua")
+-- Load Modules
+Genesis:LoadModule("UI", BASE.."ui/window.lua")
 Genesis:LoadModule("Core", BASE.."core/engine.lua")
 Genesis:LoadModule("Visual", BASE.."visual/esp.lua")
-Genesis:LoadModule("Network", BASE.."network/network.lua")
+Genesis:LoadModule("Network", BASE.."network/serverhop.lua")
 
---// Wait modules
+-- Wait modules
 repeat wait() until Genesis.Modules.UI
 repeat wait() until Genesis.Modules.Core
 
---// Start Hub
+-- Start Hub
 Genesis.Modules.UI:Init()
 
---// Tabs
+-- Tabs
 local FarmTab = Genesis.Modules.UI:CreateTab("Auto Farm")
 local CombatTab = Genesis.Modules.UI:CreateTab("Combat")
 local VisualTab = Genesis.Modules.UI:CreateTab("Visual")
 local ServerTab = Genesis.Modules.UI:CreateTab("Server")
 
---// Auto Farm
+-- Auto Farm
 Genesis.Modules.UI:CreateToggle(FarmTab,"Auto Farm",function(v)
     Genesis.Modules.Core.AutoFarm = v
     Genesis.Modules.Core:StartFarm()
 end)
 
---// Fast Attack
+-- Fast Attack
 Genesis.Modules.UI:CreateToggle(CombatTab,"Fast Attack",function(v)
     Genesis.Modules.Core.FastAttack = v
     Genesis.Modules.Core:FastAttackLoop()
 end)
 
---// ESP
+-- ESP
 Genesis.Modules.UI:CreateToggle(VisualTab,"ESP Player",function(v)
     Genesis.Modules.Visual:PlayerESP(v)
 end)
 
---// Server Hop
+-- Server Hop
 Genesis.Modules.UI:CreateButton(ServerTab,"Server Hop Low Player",function()
     Genesis.Modules.Network:HopLowServer()
 end)
