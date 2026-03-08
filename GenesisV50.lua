2e07df058ec7dacd58a75bb8210d19cb52462bf6
Genesis-scripter-osp <zatacutcanh2011@gmail.com>
2026-03-08 12:31:29 +0700

    Update GenesisV50.lua

--- GenesisV50.lua
+++ GenesisV50.lua
@@ -1,4 +1,4 @@
--- Genesis Hub Main
+-- Genesis Hub Main Loader
 
 if not game:IsLoaded() then
     game.Loaded:Wait()
@@ -6,16 +6,16 @@ end
 
 repeat task.wait() until game.Players.LocalPlayer
 
---------------------------------------------------
--- HUB
---------------------------------------------------
+------------------------------------------------
+-- HUB TABLE
+------------------------------------------------
 
 local Genesis = {}
 Genesis.Modules = {}
 
---------------------------------------------------
+------------------------------------------------
 -- MODULE LOADER
---------------------------------------------------
+------------------------------------------------
 
 function Genesis:LoadModule(name,path)
 
@@ -26,7 +26,7 @@ function Genesis:LoadModule(name,path)
         return loadstring(game:HttpGet(url))()
     end)
 
-    if success then
+    if success and result then
         self.Modules[name] = result
         print("[Genesis] Loaded:",name)
     else
@@ -35,59 +35,65 @@ function Genesis:LoadModule(name,path)
 
 end
 
---------------------------------------------------
--- CORE
---------------------------------------------------
+------------------------------------------------
+-- LOAD CORE
+------------------------------------------------
 
 Genesis:LoadModule("Engine","core/engine.lua")
 Genesis:LoadModule("Player","core/player.lua")
 Genesis:LoadModule("Services","core/services.lua")
 Genesis:LoadModule("Utils","core/utils.lua")
 
---------------------------------------------------
--- NETWORK
---------------------------------------------------
+------------------------------------------------
+-- LOAD NETWORK
+------------------------------------------------
 
 Genesis:LoadModule("Network","network/network.lua")
 
---------------------------------------------------
--- SYSTEMS
---------------------------------------------------
+------------------------------------------------
+-- LOAD SYSTEMS
+------------------------------------------------
 
 Genesis:LoadModule("AutoFarm","systems/autofarm.lua")
 Genesis:LoadModule("FastAttack","systems/fastattack.lua")
 Genesis:LoadModule("ServerHop","systems/serverhop.lua")
 
---------------------------------------------------
--- VISUAL
---------------------------------------------------
+------------------------------------------------
+-- LOAD VISUAL
+------------------------------------------------
 
 Genesis:LoadModule("ESP","visual/esp.lua")
 
---------------------------------------------------
--- UI
---------------------------------------------------
+------------------------------------------------
+-- LOAD UI
+------------------------------------------------
 
 Genesis:LoadModule("UI","ui/window.lua")
 
+------------------------------------------------
+-- WAIT UI
+------------------------------------------------
+
 repeat task.wait() until Genesis.Modules.UI
 
-Genesis.Modules.UI:Init()
+local UI = Genesis.Modules.UI
 
---------------------------------------------------
--- TABS
---------------------------------------------------
+UI:Init()
 
-local FarmTab = Genesis.Modules.UI:CreateTab("Farm")
-local CombatTab = Genesis.Modules.UI:CreateTab("Combat")
-local VisualTab = Genesis.Modules.UI:CreateTab("Visual")
-local ServerTab = Genesis.Modules.UI:CreateTab("Server")
+------------------------------------------------
+-- CREATE TABS
+------------------------------------------------
 
---------------------------------------------------
--- FARM
---------------------------------------------------
+local FarmTab = UI:CreateTab("Farm")
+local CombatTab = UI:CreateTab("Combat")
+local VisualTab = UI:CreateTab("Visual")
+local ServerTab = UI:CreateTab("Server")
 
-Genesis.Modules.UI:CreateToggle(FarmTab,"Auto Farm",function(v)
+------------------------------------------------
+-- FARM FEATURES
+------------------------------------------------
+
+FarmTab:CreateToggle("Auto Farm",function(v)
 
     if Genesis.Modules.AutoFarm then
 
@@ -101,11 +107,11 @@ Genesis.Modules.UI:CreateToggle(FarmTab,"Auto Farm",function(v)
 
 end)
 
---------------------------------------------------
--- COMBAT
---------------------------------------------------
+------------------------------------------------
+-- COMBAT FEATURES
+------------------------------------------------
 
-Genesis.Modules.UI:CreateToggle(CombatTab,"Fast Attack",function(v)
+CombatTab:CreateToggle("Fast Attack",function(v)
 
     if Genesis.Modules.FastAttack then
 
@@ -119,11 +125,11 @@ Genesis.Modules.UI:CreateToggle(CombatTab,"Fast Attack",function(v)
 
 end)
 
---------------------------------------------------
--- VISUAL
---------------------------------------------------
+------------------------------------------------
+-- VISUAL FEATURES
+------------------------------------------------
 
-Genesis.Modules.UI:CreateToggle(VisualTab,"Player ESP",function(v)
+VisualTab:CreateToggle("Player ESP",function(v)
 
     if Genesis.Modules.ESP then
         Genesis.Modules.ESP:Toggle(v)
@@ -131,11 +137,11 @@ Genesis.Modules.UI:CreateToggle(VisualTab,"Player ESP",function(v)
 
 end)
 
---------------------------------------------------
--- SERVER
---------------------------------------------------
+------------------------------------------------
+-- SERVER FEATURES
+------------------------------------------------
 
-Genesis.Modules.UI:CreateButton(ServerTab,"Server Hop",function()
+ServerTab:CreateButton("Server Hop",function()
 
     if Genesis.Modules.ServerHop then
         Genesis.Modules.ServerHop:Hop()
@@ -143,15 +149,37 @@ Genesis.Modules.UI:CreateButton(ServerTab,"Server Hop",function()
 
 end)
 
-Genesis.Modules.UI:CreateButton(ServerTab,"Rejoin Server",function()
+ServerTab:CreateButton("Rejoin Server",function()
 
     game:GetService("TeleportService"):
     Teleport(game.PlaceId,game.Players.LocalPlayer)
 
 end)
 
---------------------------------------------------
--- FINISH
---------------------------------------------------
+------------------------------------------------
+-- DEBUG TAB
+------------------------------------------------
 
-print("Genesis Hub Loaded")
+local InfoTab = UI:CreateTab("Info")
+
+InfoTab:CreateButton("Print Loaded Modules",function()
+
+    for name,_ in pairs(Genesis.Modules) do
+        print("Loaded:",name)
+    end
+
+end)
+
+InfoTab:CreateButton("Destroy Hub",function()
+
+    if UI.Destroy then
+        UI:Destroy()
+    end
+
+end)
+
+------------------------------------------------
+-- FINISH
+------------------------------------------------
+
+print("Genesis Hub Loaded Successfully")
